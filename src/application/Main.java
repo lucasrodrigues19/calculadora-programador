33c6619ab33116f16a370f8df13e0506851f2e63
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-04-25 01:35:34 -0300

    Main View

---
+++ src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/gui/MainView.fxml"));
+			Parent parent= loader.load();
+			Scene scene = new Scene(parent);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
