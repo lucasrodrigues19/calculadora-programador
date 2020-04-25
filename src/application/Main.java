886001deb279b22b405cbf4d8cb192bca2afdecd
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-04-25 02:43:02 -0300

    Design Main View

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 
@@ -12,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/gui/MainView.fxml"));
-			Parent parent= loader.load();
+			ScrollPane parent= loader.load();
 			Scene scene = new Scene(parent);
 			primaryStage.setScene(scene);
 			primaryStage.show();
