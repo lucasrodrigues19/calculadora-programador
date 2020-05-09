edcaaa0da970dc18b4d68b35705a6641c735ffd9
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-05-09 00:32:29 -0300

    Login View

--- src/view/gui/MainViewController.java
+++ src/view/gui/MainViewController.java
@@ -15,13 +15,13 @@ import javafx.stage.Stage;
 import view.gui.helper.CalculadoraHelper;
 import view.gui.utils.Alerts;
 
-public class MainViewController implements Initializable{
-	
+public class MainViewController implements Initializable {
+
 	CalculadoraHelper helper = new CalculadoraHelper();
-	
+
 	@FXML
 	private ScrollPane scrollMain;
-	
+
 	@FXML
 	private MenuItem menuInicio;
 	@FXML
@@ -29,62 +29,74 @@ public class MainViewController implements Initializable{
 
 	@FXML
 	private MenuItem menuItemSobre;
-	
+
 	@FXML
 	private MenuItem menuItemHome;
-	
-	
-	@FXML 
+
+	@FXML
 	private Button btLogin;
-	
-	@FXML 
+
+	@FXML
 	private Button btEntrar;
-	
+
 	@FXML
 	private void onBtLoginAction() {
-		System.out.println(btLogin.getText());
+		try {
+			System.out.println(btEntrar.getText());
+			helper.loadView("/view/gui/LoginView.fxml", Main.mainScene, null);
+			menuInicio.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Alerts.showAlertError(e.getMessage());
+		}
 	}
+
 	@FXML
 	private void onBtEntrarAction(ActionEvent event) {
-	
+
 		try {
 			System.out.println(btEntrar.getText());
 
 		} catch (Exception e) {
 			e.printStackTrace();
-			Alerts.showAlert("Erro", null, "Erro na view: "+e.getMessage(), AlertType.ERROR);
+			Alerts.showAlertError(e.getMessage());
 		}
 	}
+
 	@FXML
 	private void onMenuItemSobreAction(ActionEvent event) {
 		System.out.println(menuItemSobre.getText());
-	
+
 		try {
-		helper.loadViewDialog("/view/gui/AboutView.fxml",null,helper.getStageAtual(Main.mainScene));
+			helper.loadViewDialog("/view/gui/AboutView.fxml", null, helper.getStageAtual(Main.mainScene));
 		} catch (Exception e) {
 			e.printStackTrace();
-			Alerts.showAlert("Erro", null, "Erro na view: "+e.getMessage(), AlertType.ERROR);
+			Alerts.showAlertError(e.getMessage());
 		}
-		
+
 	}
+
 	@FXML
 	private void onMenuItemHomeAction() {
 		System.out.println(menuInicio.getText());
 		try {
-		helper.loadMainView("/view/gui/MainView.fxml",Main.mainScene);
+			helper.loadMainView("/view/gui/MainView.fxml", Main.mainScene);
+			menuInicio.setVisible(false);
 		} catch (Exception e) {
-			Alerts.showAlert("Erro", null, "Erro na view: "+e.getMessage(), AlertType.ERROR);
+			Alerts.showAlertError(e.getMessage());
 		}
-		
+
 	}
 
 	public void setWithHeigt() {
 		scrollMain.setFitToHeight(true);
 		scrollMain.setFitToWidth(true);
 	}
+
 	public void hidenControls() {
 		menuInicio.setVisible(false);
 	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		hidenControls();
