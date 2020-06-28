c97fd363b724ea18ab56fd4799dc89da01bd605c
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-06-28 02:39:52 -0300

    Funções convertBinToDec, convertDecToBin e CompletarBinario

--- src/conversor_numerico/modelo/helpers/ConversorHelper.java
+++ src/conversor_numerico/modelo/helpers/ConversorHelper.java
@@ -18,5 +18,66 @@ public class ConversorHelper {
 		
 	}
 	
+	public String convertDecToBin() {
+		int resto = 0;
+		int n = 0;
+		String res = "";
+		if(TryParseUtils.tryParseInt(dadosEntrada.getStrNum()) != null)
+			n = TryParseUtils.tryParseInt(dadosEntrada.getStrNum());
+		
+		while(n > 0) {
+			resto = n % 2;
+			pilha.push(resto);
+			n/=2;
+		}
+		
+		while(!pilha.isEmpty()) {
+			n = pilha.pop();
+			res+= Integer.toString(n);
+		}
+		return completarBinario(res);
+	}
+	public String convertBinToDec() {
+		String str = dadosEntrada.getStrNum();
+		int length = str.length() -1;
+		int n = 0;
+		double soma = 0;
+		for(int i  = length; i>=0;i--) {
+			double  num = Math.pow(2, n);
+			if(str.charAt(i) == '1')
+				soma+= num;
+			n++;
+		}
 	
+		return Double.toString(soma);
+	}
+	private String completarBinario(String str) {
+		if(str != null) {
+			switch(str) {
+			case "1":
+				str = "000"+str;
+				break;
+			case "10":
+				str = "00"+str;
+			 	break;
+			case "11":
+				str = "00"+str;
+				break;
+			case "100":
+				str = "0"+str;
+				break;
+			case "101":
+				str = "0"+str;
+				break;
+			case "110":
+				str = "0"+str;
+				break;
+			case "111":
+				str = "0"+str;
+				break;
+			}
+		}
+		return str;
+		
+	}
 }
